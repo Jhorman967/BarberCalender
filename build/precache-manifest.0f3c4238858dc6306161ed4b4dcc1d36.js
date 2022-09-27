06f92d21795fd03fdbc3586642bbdbcd5671be36
Jhorman967 <jhorman967@gmail.com>
2022-09-27 12:01:39 -0500

    secon commit

---
+++ build/precache-manifest.0f3c4238858dc6306161ed4b4dcc1d36.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b3d158254939f8bbc6b3e808f05333",
+    "url": "/BarberCalender/index.html"
+  },
+  {
+    "revision": "8c4042d43f659ab999fd",
+    "url": "/BarberCalender/static/css/2.320fd0de.chunk.css"
+  },
+  {
+    "revision": "c48b8ae84988b0e63e84",
+    "url": "/BarberCalender/static/css/main.e2644493.chunk.css"
+  },
+  {
+    "revision": "8c4042d43f659ab999fd",
+    "url": "/BarberCalender/static/js/2.4eaa0cda.chunk.js"
+  },
+  {
+    "revision": "8d4c639e44d63bdceaa0183658bd97e0",
+    "url": "/BarberCalender/static/js/2.4eaa0cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac8a895dbc4be7f8de1",
+    "url": "/BarberCalender/static/js/3.6c1a3410.chunk.js"
+  },
+  {
+    "revision": "8076a39f2c6eb4b606ff15bd0342709c",
+    "url": "/BarberCalender/static/js/3.6c1a3410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48b8ae84988b0e63e84",
+    "url": "/BarberCalender/static/js/main.16a13573.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/BarberCalender/static/js/main.16a13573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068652064954fec06534",
+    "url": "/BarberCalender/static/js/runtime-main.080af0e5.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "/BarberCalender/static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/BarberCalender/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/BarberCalender/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/BarberCalender/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/BarberCalender/static/media/primeicons.e01fd413.eot"
+  }
+]);
